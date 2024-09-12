9f15741caa451ae854fc86e0bc83cdea3498a306
Daniil Bykov <daniil0604bykiv@gmail.com>
2024-09-12 22:07:00 +0300

    consumer

--- internal/clients/telegram/telegram.go
+++ internal/clients/telegram/telegram.go
@@ -21,8 +21,8 @@ type Client struct {
 	client   http.Client
 }
 
-func New(host string, token string) Client {
-	return Client{
+func New(host string, token string) *Client {
+	return &Client{
 		host:     host,
 		basePath: NewBasePath(token),
 		client:   http.Client{},
