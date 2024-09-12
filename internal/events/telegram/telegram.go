9509d890cc52e9d91d0bff3a3414dcb16cc4c525
Daniil Bykov <daniil0604bykiv@gmail.com>
2024-09-12 16:29:48 +0300

    storage and file storage

--- internal/events/telegram/telegram.go
+++ internal/events/telegram/telegram.go
@@ -1,1 +1,11 @@
 package telegram
+
+import "github.com/Dnlbb/telegram-bot/internal/clients/telegram"
+
+type Processor struct {
+	tg     *telegram.Client
+	offset int
+	// stor
+}
+
+// func New(client *telegram.Client, stor storage )
